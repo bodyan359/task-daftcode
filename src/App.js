bc818908683e7abe9ba728b33ef36cf3763db06c
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-05-20 20:21:00 +0200

    Add Pluses Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 
-import { Hero, Header, Footer } from "./components/index";
+import { Hero, Header, Footer, Pluses } from "./components/index";
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <Hero />
+      {/* <Hero /> */}
+      <Pluses />
       {/* <Footer /> */}
     </div>
   );
---
+++ src/components/Pluses.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Pluses() {
+  return (
+    <div className="Pluses">
+      <div> 230+ Scientifically based tests</div>
+      <div> 10,000+ Medical recommendations</div>
+      <div> 25M+ Students learning</div>
+    </div>
+  );
+}
+
+export default Pluses;
