4c60e17b854e6084acccab4b1f25b09d2d767119
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-05-20 20:22:18 +0200

    Add CommunitySating Component

---
+++ src/components/CommunitySaying.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CommunitySaying() {
+  return (
+    <div>
+      <div>What our community is saying?</div>
+      <div>
+        A mission-driven company that invest in and builds healthier living
+      </div>
+      <div> VIDEO </div>
+    </div>
+  );
+}
+
+export default CommunitySaying;
