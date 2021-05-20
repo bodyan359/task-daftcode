0b09ef951bd1cc858a8aaaf185608142b524c913
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-05-20 16:58:46 +0200

    Create Header element Add img folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../img/Logo.png";
 
 function Header() {
-  return <div className="Header">Header</div>;
+  let MENU_ITEMS = ["Home", "About", "Courses", "News", "Contact", "Log in"];
+  return (
+    <div className="Header">
+      <img src={logo} className="CompanyLogo" alt="Logo" />
+      <div className="TopMenu">
+        {MENU_ITEMS.map((menu_item) => (
+          <div className={menu_item === "Log in" ? "LoginButton" : ""}>
+            {menu_item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
