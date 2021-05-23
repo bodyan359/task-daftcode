41ad28021016366faabba2162f1a80988cd7fd54
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-05-24 01:26:15 +0200

    Fotter add RWD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,16 +57,18 @@ function Footer() {
           </div>
         </div>
       </div>
-      <svg
-        width="100%"
-        height="1"
-        viewBox="0 0 1110 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect opacity="0.2" width="1110" height="1" fill="white" />
-      </svg>
-      <p className="footerText posRight">Terms & Conditions</p>
+      <div className="termsAndConditions">
+        <svg
+          width="100%"
+          height="1"
+          viewBox="0 0 1110 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect opacity="0.2" width="1110" height="1" fill="white" />
+        </svg>
+        <p className="footerText posRight">Terms & Conditions</p>
+      </div>
     </div>
   );
 }
